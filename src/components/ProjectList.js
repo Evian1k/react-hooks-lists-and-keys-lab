f5438cd009d49b54428c6f13cb4f0f471835c932
Evian1k <emmanuelevian@gmail.com>
2025-04-22 15:08:38 +0300

    update

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,35 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
-function ProjectList({ projects }) {
-  console.log(projects);
+function ProjectList() {
+  const projects = [
+    {
+      name: "Project 1",
+      about: "This is a description of Project 1.",
+      technologies: ["React", "JavaScript", "CSS"],
+    },
+    {
+      name: "Project 2",
+      about: "This is a description of Project 2.",
+      technologies: ["Node.js", "Express", "MongoDB"],
+    },
+    {
+      name: "Project 3",
+      about: "This is a description of Project 3.",
+      technologies: ["Python", "Flask", "SQL"],
+    },
+  ];
+
   return (
-    <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+    <div>
+      {projects.map((project, index) => (
+        <ProjectItem
+          key={index}
+          name={project.name}
+          about={project.about}
+          technologies={project.technologies}
+        />
+      ))}
     </div>
   );
 }
